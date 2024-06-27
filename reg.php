54ccc56e27de9f86f09f522212619a5fd8d56921
WhiteFaceVarre <prokopovaleksandr544@gmail.com>
2024-06-27 12:29:04 +0700

    feat validation data from client
    (username + phone number + email + password)

--- reg.php
+++ reg.php
@@ -3,16 +3,47 @@
 $userName = $_POST['user_name'];
 $phoneNumber = $_POST['user_phone_number'];
 $email = $_POST['user_email'];
-$password = $_POST['user_password'];
 
-$document_lines = file("./db/users.txt");
+$password = urlencode($_POST['user_password']);
 
-$lastUser = end($document_lines);
+if (strlen($userName) == 0 || strlen($userName) < 16) {
+    echo "Короткое имя: ФИО не должно содержать менее 16 символов.";
+}
+else if (!preg_match('/^[а-яёА-ЯЁ\s]+$/u', $userName)) {
+    echo "Некорректное имя: ФИО должно содержать только русские буквы, без символов.";
+}
+else {
+    if(!preg_match("/^[0-9]{10,11}+$/", $phoneNumber)) {
+        echo "Неправильный номер телефона: присутствуют иные символы или некорректная длина номера.";
+    }
+    else if ($phoneNumber[0] != 7) {
+        echo "Неправильный номер телефона: первая цифра должна быть 7.";
+    }
+    else {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Некорректный адрес электронной почты: неверная структура адреса.";
+        }
+        else {
+            if (strlen($password) < 8) {
+                echo "Некорректный пароль: пароль должен содержать не менее 8 символов.";
+            }
+            else {
+                $salt_crypt = md5($password);
+                $password_crypt = crypt($password, $salt_crypt);
+                
+                $document_lines = file("./db/users.txt");
+                
+                $lastUser = end($document_lines);
+                
+                preg_match('/ID:\s*(\d+)\b/', $lastUser, $lastUserId);
+                
+                $currentUserId = (int)$lastUserId[1] + 1;
+                
+                $user_data = "\n" . 'ID: ' . $currentUserId . ' | Name: ' . $userName . ' | Phone Number: ' . $phoneNumber . ' | Email: ' . $email . ' | Password: ' . $password_crypt;
+                
+                file_put_contents("./db/users.txt", $user_data, FILE_APPEND);
+            }
+        }
+    }
+}
 
-preg_match('/ID:\s*(\d+)\b/', $lastUser, $lastUserId);
-
-$currentUserId = (int)$lastUserId[1] + 1;
-
-$user_data = "\n" . 'ID: ' . $currentUserId . ' | Name: ' . $userName . ' | Phone Number: ' . $phoneNumber . ' | Email: ' . $email . ' | Password: ' . $password;
-
-file_put_contents("./db/users.txt", $user_data, FILE_APPEND);
